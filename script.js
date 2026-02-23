27233f0ac2e1e3d04a34f98516016bd34bfb7225
Kevin Fernando <kevinchanaka1@gmail.com>
2026-02-24 08:39:24 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const weddingDate = new Date(1792578600000).getTime();
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.querySelector(".countdown").innerHTML =
+      "<h3>We're Married! 🎉</h3>";
+  }
+}, 1000);
